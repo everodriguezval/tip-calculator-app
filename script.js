f44460567ffee1e6366b88b3ca23c171b40dae36
everodriguezval <eve.rodriguezval@gmail.com>
2023-04-03 16:54:32 +0100

    getTipValue function added

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ const btnContainer = getElement('.btn-tip-container');
 const numberOfPeople = getElement('.input-number-people');
 const customTip = getElement('#custom-tip');
 const resetBtn = getElement('#reset-btn');
+const tipAmount = getElement('span:first-child');
+const total = getElement('span:last-child');
 
-// console.log(inputBill);
-// console.log(btnContainer);
-// console.log(numberOfPeople);
-// console.log(customTip);
-// console.log(resetBtn);
+
+inputBill.addEventListener('input', (e) => {
+    let inputBillValue = Number(e.target.value);
+});
+
+btnContainer.addEventListener('click', getTipValue);
+
+numberOfPeople.addEventListener('input', (e) => {
+    console.log(e.target.value);
+});
+
+resetBtn.addEventListener('click', (e) => {
+    console.log(e.target.value);
+});
 
 
 function getElement(identifier) {
@@ -17,5 +28,21 @@ function getElement(identifier) {
         throw new Error('Could not find the element specified')
     } else {
         return el;
+    }
+}
+
+function getTipValue(e) {
+    let tipPercentage = e.target.value;
+
+    if (tipPercentage != 0) {
+        console.log('A number but with a $!')
+        let tipNumber = Number(tipPercentage.substring(0, tipPercentage.length - 1));       
+        return tipNumber;
+    } else {
+        console.log('User selected custom tip');
+        customTip.addEventListener('input', (e) => {
+            let customTipNumber = Number(e.target.value);           
+            return customTipNumber;
+        });       
     }
 }

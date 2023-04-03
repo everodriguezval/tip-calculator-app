fd0519e6806769ff845edfeade4eb19430617ec2
everodriguezval <eve.rodriguezval@gmail.com>
2023-04-03 19:19:59 +0100

    extra functions added such as getNumberOfPeople and tipCalculation

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
+// Selecting elements
 const inputBill = getElement('.input-bill');
 const btnContainer = getElement('.btn-tip-container');
-const numberOfPeople = getElement('.input-number-people');
+const inputPeople = getElement('.input-number-people');
 const customTip = getElement('#custom-tip');
 const resetBtn = getElement('#reset-btn');
 const tipAmount = getElement('span:first-child');
 const total = getElement('span:last-child');
 
+// Adding event listeners
+inputBill.addEventListener('input', getBillNumber);
+btnContainer.addEventListener('click', getTipNumber);
+inputPeople.addEventListener('input', getNumberOfPeople);
+resetBtn.addEventListener('click', reset);
 
-inputBill.addEventListener('input', (e) => {
-    let inputBillValue = Number(e.target.value);
-});
+// Creating variables
+let inputBillValue, tipNumber, numberOfpeople;
 
-btnContainer.addEventListener('click', getTipValue);
+// Customising clicked buttons
+const buttonItems = Array.from(btnContainer.children);
+buttonItems.forEach(button => {
+    button.addEventListener('click', (event) => {
+        if (event.target.classList.contains('filtered')) {
+            event.target.classList.toggle('tip-selected');
+        } 
+    });
+}); 
 
-numberOfPeople.addEventListener('input', (e) => {
-    console.log(e.target.value);
-});
-
-resetBtn.addEventListener('click', (e) => {
-    console.log(e.target.value);
-});
-
-
+// Selecting element from the DOM function
 function getElement(identifier) {
     const el = document.querySelector(identifier);
     if (!el) {
@@ -31,18 +36,44 @@ function getElement(identifier) {
     }
 }
 
-function getTipValue(e) {
-    let tipPercentage = e.target.value;
+// Grabbing bill value function
+function getBillNumber(e) {
+    inputBillValue = Number(e.target.value);
+    console.log(inputBillValue);
+    return inputBillValue;
+}
 
+// Grabbing tip value function
+function getTipNumber(e) {
+    let tipPercentage = e.target.value;
     if (tipPercentage != 0) {
-        console.log('A number but with a $!')
-        let tipNumber = Number(tipPercentage.substring(0, tipPercentage.length - 1));       
+        tipNumber = Number(tipPercentage.substring(0, tipPercentage.length - 1));       
+        console.log(tipNumber);
         return tipNumber;
     } else {
-        console.log('User selected custom tip');
+        // console.log('User selected custom tip');
         customTip.addEventListener('input', (e) => {
-            let customTipNumber = Number(e.target.value);           
-            return customTipNumber;
+            tipNumber = Number(e.target.value);           
+            return tipNumber;
         });       
     }
 }
+
+// Grabbing number of people function
+function getNumberOfPeople(e) {
+    numberOfpeople = Number(e.target.value);
+    // console.log(numberOfpeople);
+    tipCalculation();
+    return numberOfpeople;
+}
+
+function tipCalculation() {
+    tipTotal = inputBillValue * ((tipNumber / 100).toFixed(2)) / numberOfpeople;
+    // console.log(tipTotal);
+}
+
+function reset() {
+ console.log('reset')
+}
+
+
